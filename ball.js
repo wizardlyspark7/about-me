ae27dcef5f43b526fb309b245dedc85c3e5ecca4
Zach I <zach.irain17@gmail.com>
2025-06-23 21:11:26 -0600

    fixes

--- ball.js
+++ ball.js
@@ -4,21 +4,24 @@ document.body.appendChild(ball);
 let modulo = 60;
 let counter = 0;
 
+
+const x = Math.random() * window.innerWidth;
+const y = Math.random() * window.innerHeight;
+ball.style.position = `absolute`;
+ball.style.width = `50px`;
+ball.style.height = `50px`;
+ball.style.backgroundColor = `red`;
+ball.style.borderRadius = `50%`;
+
 function moveBall() {
     counter++;
     if (counter % modulo === 0)
     {
         const x = Math.random() * window.innerWidth;
         const y = Math.random() * window.innerHeight;
+        ball.style.left = `${x}px`;
+        ball.style.top = `${y}px`;
     }
-    ball.style.left = `${x}px`;
-    ball.style.top = `${y}px`;
-    ball.style.position = `absolute`;
-    ball.style.width = `50px`;
-    ball.style.height = `50px`;
-    ball.style.backgroundColor = `red`;
-    ball.style.borderRadius = `50%`;
-
     requestAnimationFrame(moveBall);
 }
 
